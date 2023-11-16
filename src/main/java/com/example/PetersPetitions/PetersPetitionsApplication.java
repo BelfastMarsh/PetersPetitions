0c13f41cd7518173289802155c5154916fd2237b
Peter <peteremarsh@gmail.com>
2023-11-16 12:25:26 +0000

    Initial commit

---
+++ src/main/java/com/example/PetersPetitions/PetersPetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetersPetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetersPetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetersPetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PetersPetitions/PetersPetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetersPetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetersPetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
