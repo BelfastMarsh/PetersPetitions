a0324d7e69e860beec4f85cc0e2cc9c03ccacc8c
Peter <peteremarsh@gmail.com>
2023-11-17 21:49:52 +0000

    Added search.master, search-result.html and /search url to take search items from users.

---
+++ src/main/java/com/example/PetersPetitions/WeightedPetition.java
@@ -0,0 +1,22 @@
+package com.example.PetersPetitions;
+
+public class WeightedPetition {
+    private int weighting;
+    private Petition petition;
+
+    public WeightedPetition(int weighting, Petition petition) {
+        this.petition = petition;
+        this.weighting = weighting;
+    }
+
+    public Petition getPetition() {
+        return petition;
+    }
+
+    public int getWeighting(){
+        return weighting;
+    }
+    public void addWeighting(int weighting){
+        this.weighting += weighting;
+    }
+}
