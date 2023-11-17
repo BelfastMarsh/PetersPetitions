a2865127ab4e6dd06e44201f9a6798d7660ca925
Peter <peteremarsh@gmail.com>
2023-11-17 19:41:59 +0000

    Added fragments.html Petition class, User class and added placeholder mappings for site functionality to Controller.java

--- src/main/java/com/example/PetersPetitions/Controller.java
+++ src/main/java/com/example/PetersPetitions/Controller.java
@@ -1,8 +1,41 @@
 package com.example.PetersPetitions;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class Controller {
-    @RequestMapping("/")
-    public String hello1() {return "index";}
+
+    private void coreActions(Model model){
+        model.addAttribute("petitions", Petition.getPetitions());
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        this.coreActions(model);
+        model.addAttribute("title", "View All Petitions");
+        return "index";
+    }
+
+    @GetMapping(value = "/view")
+    public String view(Model model){
+        this.coreActions(model);
+        model.addAttribute("title", "View All Petitions");
+        return "index";
+    }
+
+    @GetMapping(value = "/create")
+    public String create(Model model){
+        this.coreActions(model);
+        model.addAttribute("title", "Create Petition");
+        return "index";
+    }
+
+    @GetMapping(value = "/search")
+    public String search(Model model){
+        this.coreActions(model);
+        model.addAttribute("title", "Search Petitions");
+        return "index";
+    }
+
 }
---
+++ src/main/java/com/example/PetersPetitions/User.java
@@ -0,0 +1,10 @@
+package com.example.PetersPetitions;
+
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private ArrayList<Petition> petitions;
+    private ArrayList<Petition> signedPetitions;
+
+}
