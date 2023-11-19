b1b87cf3c50c2d2ac9ea90850014a7ff1c33ee68
Peter <peteremarsh@gmail.com>
2023-11-19 08:40:04 +0000

    Styling completed for home, create, view petition.

--- src/main/java/com/example/PetersPetitions/PetersPetitionsApplication.java
+++ src/main/java/com/example/PetersPetitions/PetersPetitionsApplication.java
@@ -3,11 +3,20 @@ package com.example.PetersPetitions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class PetersPetitionsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetersPetitionsApplication.class, args);
+		// added in the port change for local in-development and revert (manually) revert to run statement before pushing to Git
+		SpringApplication.run(PetersPetitionsApplication.class, args);/*
+
+		SpringApplication app = new SpringApplication(PetersPetitionsApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port","9095"));
+		app.run(args);
+		//*/
+
 	}
 
 }
