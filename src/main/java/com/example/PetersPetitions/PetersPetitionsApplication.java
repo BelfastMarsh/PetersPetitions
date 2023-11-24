e4badd6c4b4cbd26e86dde69d9152368f60037f9
Peter <peteremarsh@gmail.com>
2023-11-24 17:59:03 +0000

    reverted PetersPetitionsApplication.main to standard run method call.

--- src/main/java/com/example/PetersPetitions/PetersPetitionsApplication.java
+++ src/main/java/com/example/PetersPetitions/PetersPetitionsApplication.java
@@ -10,12 +10,7 @@ public class PetersPetitionsApplication {
 
 	public static void main(String[] args) {
 		// added in the port change for local in-development and revert (manually) revert to run statement before pushing to Git
-		//SpringApplication.run(PetersPetitionsApplication.class, args);/*
-
-		SpringApplication app = new SpringApplication(PetersPetitionsApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port","9095"));
-		app.run(args);
-		//*/
+		SpringApplication.run(PetersPetitionsApplication.class, args);
 
 	}
 
